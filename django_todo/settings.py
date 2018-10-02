022bb9d40eeea8a92efe5069ea76c67ac4c42c9a
Liam-Cafearo <lcafearo@gmail.com>
2018-10-02 21:37:45 +0100

    added cleardb details

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -93,10 +93,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 
