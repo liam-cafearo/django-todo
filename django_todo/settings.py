e06d932a2407c45aa5877ef6c76f40d8a70fd884
Liam-Cafearo <lcafearo@gmail.com>
2018-10-03 06:59:07 +0100

    corrected formatting

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'gyf1^wrz#z#353@h!38wd+di&vajvw1hirjo+p19!wweh*1_6!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "django-todo-liamcafearo.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost",
+                 "django-todo-liamcafearo.herokuapp.com"]
 
 
 # Application definition
